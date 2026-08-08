b7494f8716de4be427bd21038d30e41c861634c0
Yahya nageh <yahyan.nageh@gmail.com>
2026-08-08 21:47:09 +0300

    Create Transaction Details Domain Layer

---
+++ app/src/main/java/com/example/e_commerceapp/transaction_details/domain/model/SectionDomainModel.kt
@@ -0,0 +1,7 @@
+package com.example.e_commerceapp.transaction_details.domain.model
+
+data class SectionDomainModel(
+    val key: String?,
+    val title: String?,
+    val rows: List<RowsDomainModel>?
+)
---
+++ app/src/main/java/com/example/e_commerceapp/transaction_details/domain/repository/TransactionDetailsRepository.kt
@@ -0,0 +1,8 @@
+package com.example.e_commerceapp.transaction_details.domain.repository
+
+import com.example.e_commerceapp.transaction_details.domain.model.TransactionDetailsDomainModel
+import com.example.e_commerceapp.transaction_list.domain.model.ApiResultStatus
+
+interface TransactionDetailsRepository {
+    suspend fun getTransactionById(id: Int): ApiResultStatus<TransactionDetailsDomainModel?>
+}
