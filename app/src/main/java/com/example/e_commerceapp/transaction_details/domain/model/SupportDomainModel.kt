af31fbc94ac5a53f94ccdb3c7bcef4b8e54f65ac
Yahya nageh <yahyan.nageh@gmail.com>
2026-08-12 14:55:48 +0300

    Apply Aziz Comments REMOVE Nullability from Domain Layer

--- app/src/main/java/com/example/e_commerceapp/transaction_details/domain/model/SupportDomainModel.kt
+++ app/src/main/java/com/example/e_commerceapp/transaction_details/domain/model/SupportDomainModel.kt
@@ -1,6 +1,6 @@
 package com.example.e_commerceapp.transaction_details.domain.model
 
 data class SupportDomainModel(
-    val text: String?,
-    val actionLabel: String?
+    val text: String,
+    val actionLabel: String
 )
