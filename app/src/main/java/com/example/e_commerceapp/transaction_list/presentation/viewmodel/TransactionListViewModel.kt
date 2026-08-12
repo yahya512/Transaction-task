a228ebfb81804a0d4e57ddccdad45388b3e573c8
Yahya nageh <yahyan.nageh@gmail.com>
2026-08-12 11:50:43 +0300

    updated Presentation layer TransactionListViewModel.kt

--- app/src/main/java/com/example/e_commerceapp/transaction_list/presentation/viewmodel/TransactionListViewModel.kt
+++ app/src/main/java/com/example/e_commerceapp/transaction_list/presentation/viewmodel/TransactionListViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.e_commerceapp.transaction_list.presentation.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.e_commerceapp.transaction_list.domain.mapper.PaginationUiMapper
